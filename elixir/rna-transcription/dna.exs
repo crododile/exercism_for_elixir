bc06f315930d4989df26290477e1580cb0c4a910
crododile <coburn.d.berry@gmail.com>
2015-11-27 15:18:56 -0600

    rna transcription

---
+++ elixir/rna-transcription/dna.exs
@@ -0,0 +1,18 @@
+defmodule DNA do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> DNA.to_rna('ACTG')
+  'UGAC'
+  """
+  @mapping %{?G => 'C', ?C => 'G', ?T => 'A', ?A => 'U'}
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map( dna, &Dict.get(@mapping, &1))
+    |> Enum.join("")
+    |> to_char_list
+  end
+end
