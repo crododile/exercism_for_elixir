3506ef51823bffda7ea515f8219abf8d154c7ce8
crododile <coburn.d.berry@gmail.com>
2015-11-26 23:17:46 -0600

    nucleotide count

---
+++ elixir/nucleotide-count/dna.exs
@@ -0,0 +1,42 @@
+defmodule DNA do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> DNA.count('AATAA', ?A)
+  4
+
+  iex> DNA.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    strand
+    |> Enum.count fn(codepoint) -> codepoint === nucleotide end
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> DNA.nucleotide_counts('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec nucleotide_counts([char]) :: Dict.t
+  def nucleotide_counts(strand) do
+    @nucleotides
+    |> Enum.map(&reduce_and_count(&1, strand))
+    |> Enum.into %{}
+  end
+  
+  defp reduce_and_count(codepoint, strand) do
+    {codepoint, count(strand, codepoint)}
+  end
+
+
+end
